846b0d21871c3d5c0a5fcc8edd4d85e231ab76c6
AJGamess <ajacoboski@student.neumont.edu>
2026-07-30 16:43:44 -0600

    Added GameManager Script

---
+++ Assets/PropulsionPistol/Scripts/GameManager.cs
@@ -0,0 +1,78 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private GameObject mainMenuUI;
+    [SerializeField] private GameObject gameUI;
+    [SerializeField] private GameObject timerText;
+
+    GameState currentGameState = GameState.Playing;
+    float timer = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check current game state
+        switch (currentGameState)
+        {
+            case GameState.MainMenu:
+                // Handle main menu logic
+                break;
+            case GameState.Playing:
+                // Handle gameplay logic
+                // When Player presses the reset button, reset the level
+                if (Input.GetKeyDown(KeyCode.R))
+                {
+                    ResetLevel();
+                }
+                //When Player presses the pause button, pause the game
+                if (Input.GetKeyDown(KeyCode.P))
+                {
+                    currentGameState = GameState.Paused;
+                    Time.timeScale = 0f; // Pause the game
+                }
+                break;
+            case GameState.Paused:
+                // Handle pause menu logic
+                // When Player presses the pause button or clicks resume button, resume the game
+                if (Input.GetKeyDown(KeyCode.P)) //TODO: Add a resume button to the pause menu and call this function when the button is clicked
+                {
+                    currentGameState = GameState.Playing;
+                    Time.timeScale = 1f; // Resume the game
+                }
+                break;
+            case GameState.GameOver:
+                // Handle game over logic
+                break;
+        }
+        
+    }
+
+    enum GameState
+    {
+        MainMenu,
+        Playing,
+        Paused,
+        GameOver
+    }
+
+    void ResetLevel()
+    {
+        // Reload the current scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+
+    }
+    void Timer()
+    {
+        //Create a timer that counts up in seconds and miliseconds and displays it on the screen with a UI Text element
+        timer += Time.deltaTime;
+
+    }
+}
